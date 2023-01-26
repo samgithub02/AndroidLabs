0cbb4c679b32dffb62b99b4828318240f687bd30
samarth sharma <samarthsharma702@gmail.com>
2023-01-25 21:53:00 -0500

    Initial commit

---
+++ app/src/main/java/com/com/cst2335/shar0686/MainActivity.java
@@ -0,0 +1,14 @@
+package com.com.cst2335.shar0686;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
