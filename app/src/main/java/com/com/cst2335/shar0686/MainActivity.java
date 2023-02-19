7fc2fd384f10a0c89e13819f89f8973591cd1a44
Samarth <samarthsharma702@gmail.com>
2023-02-19 17:17:10 -0500

    Finished Lab4

--- app/src/main/java/com/com/cst2335/shar0686/MainActivity.java
+++ app/src/main/java/com/com/cst2335/shar0686/MainActivity.java
@@ -7,18 +7,18 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 public class MainActivity extends AppCompatActivity {
-    private CheckBox cb;
+    private CheckBox checkBox;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
+        setContentView(R.layout.activity_profile_activity);
         EditText emailEditText = findViewById(R.id.editTextTextEmailAddress);
         Button loginButton = findViewById(R.id.button);
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email_address = emailEditText.getText().toString();
-                Intent goToProfile = new Intent(MainActivity.this, ProfileActivity.class);
+                Intent goToProfile = new Intent(MainActivity.this,ProfileActivity.class);
                 goToProfile.putExtra("EMAIL", email_address);
                 startActivity(goToProfile);
             }
--- app/src/main/java/com/com/cst2335/shar0686/ProfileActivity.java
+++ app/src/main/java/com/com/cst2335/shar0686/ProfileActivity.java
@@ -1,104 +1,65 @@
 package com.com.cst2335.shar0686;
-
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
-
-
-
 public class ProfileActivity extends AppCompatActivity {
-    ;
-    private ActivityResultLauncher<Intent> myPictureTakerLauncher;
-
-    private static final String TAG = "ProfileActivity";
-
+    public String NAME = "text";
+    public String ADDRESS = "address";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile_activity);
-        EditText emailEditText = findViewById(R.id.editTextTextEmailAddress2);
+        setContentView(R.layout.activity_profile);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("shared", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        EditText name = findViewById(R.id.Name);
+        EditText address = findViewById(R.id.Address);
+        TextView emailEditText = findViewById(R.id.emailAddress);
+
+        Button saveButton = findViewById(R.id.button2);
+        Button ClearButton = findViewById(R.id.button3);
 
         Intent intent = getIntent();
         String email = intent.getStringExtra("EMAIL");
         emailEditText.setText(email);
 
-        Button takePictureButton = findViewById(R.id.button2);
+        name.setText(sharedPreferences.getString(NAME, ""));
+        address.setText(sharedPreferences.getString(ADDRESS, ""));
 
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editor.putString(NAME, name.getText().toString());
+                editor.putString(ADDRESS, address.getText().toString());
+                editor.apply();
+            }
+        });
 
+        ClearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                editor.clear();
+                editor.commit();
+                name.setText("");
+                address.setText("");
 
-        takePictureButton.setOnClickListener(v -> dispatchTakePictureIntent());
-
-        myPictureTakerLauncher = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult result) {
-                        ImageView imgView = findViewById(R.id.imageView);
-                        if (result.getResultCode() == Activity.RESULT_OK) {
-                            Intent data = result.getData();
-                            assert data != null;
-                            Bitmap imgBitmap = (Bitmap) data.getExtras().get("data");
-                            imgView.setImageBitmap(imgBitmap);
-                        } else if (result.getResultCode() == Activity.RESULT_CANCELED) {
-                            Log.i(TAG, "User refused to capture a picture.");
-                        }
-                    }
-                }
-        );
+            }
+        });
     }
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            myPictureTakerLauncher.launch(takePictureIntent);
-        }
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.e(TAG, "onStart method");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.e(TAG, "onResume method");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.e(TAG, "onPause method");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.e(TAG, "onStop method");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.e(TAG, "onDestroy method");
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        Log.e(TAG, "onActivityResult method");
-    }
-
 }
